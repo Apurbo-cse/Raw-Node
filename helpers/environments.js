61b5239cf5e75cf73f3b8357ab50c43b458e47af
Apurbo Kumar Anup <apurboka@gmail.com>
2022-11-20 16:45:59 +0600

    environmentToExport

---
+++ helpers/environments.js
@@ -0,0 +1,28 @@
+
+
+// dependencies
+
+
+// modules scaffolding
+
+const environments = {};
+
+environments.staging = {
+     port : 3000,
+     envName : 'staging'
+}
+
+environments.production = {
+     port : 5000,
+     envName : 'production'
+}
+
+
+// determine which environments was passed
+const currentEnvironments = typeof(process.env.NODE_ENV) ===  'string' ? process.env.NODE_ENV : 'staging',
+
+
+// export corresponding environment object
+const environmentToExport = typeof(environments[currentEnvironments]) === 'object' ? environments[currentEnvironments] : environments.staging;
+
+module.exports = environmentToExport ;
--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -36,27 +36,28 @@ handler.handleReqRes = (req, res) => {
     ? routes[trimmedPath]
     : notFoundHandler;
 
-  chosenHandler(requestProperties, (statusCode, payload) => {
-    statusCode = typeof statusCode === "number" ? statusCode : 500;
-    payload = typeof payload === "object" ? payload : {};
-
-    const payloadString = JSON.stringify(payload);
-
-    // return the final response
-    res.writeHead(statusCode);
-    res.end(payloadString);
-  });
-
   req.on("data", (buffer) => {
     realData += decoder.write(buffer);
   });
 
   req.on("end", () => {
     realData += decoder.end();
-    console.log(realData);
+    
+    //Chosen handler
+    chosenHandler(requestProperties, (statusCode, payload) => {
+      statusCode = typeof statusCode === "number" ? statusCode : 500;
+      payload = typeof payload === "object" ? payload : {};
+
+      const payloadString = JSON.stringify(payload);
+
+      // return the final response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    });
 
     // response handle
     res.end("Hello World!");
+    
   });
 
   console.log(headersObject);
